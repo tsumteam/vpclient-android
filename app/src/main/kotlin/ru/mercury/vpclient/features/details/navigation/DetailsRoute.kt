df103485daa92fe5de53c42c5618fba9f7e72412
michaelbel <michael-bel@outlook.com>
2026-05-18 17:48:01 +0300

    Fix details route comment

--- app/src/main/kotlin/ru/mercury/vpclient/features/details/navigation/DetailsRoute.kt
+++ app/src/main/kotlin/ru/mercury/vpclient/features/details/navigation/DetailsRoute.kt
@@ -3,7 +3,7 @@ package ru.mercury.vpclient.features.details.navigation
 import androidx.navigation3.runtime.NavKey
 import kotlinx.serialization.Serializable
 
-// не data-class чтобы открывать одинаковые экраны в nav3
+// не data class, чтобы открывать одинаковые экраны в nav3
 @Serializable
 class DetailsRoute(
     val id: String,
