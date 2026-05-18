a63fcd079725f9435e4669120c30dac8a7ddd145
michaelbel <michael-bel@outlook.com>
2026-05-18 11:50:54 +0300

    Remove unused UI ktx helpers

--- app/src/main/kotlin/ru/mercury/vpclient/shared/ui/ktx/LifecycleKtx.kt
+++ app/src/main/kotlin/ru/mercury/vpclient/shared/ui/ktx/LifecycleKtx.kt
@@ -1,12 +1,8 @@
 package ru.mercury.vpclient.shared.ui.ktx
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.Dispatchers
@@ -25,36 +21,6 @@ fun <T> ObserveAsEvents(
             withContext(Dispatchers.Main.immediate) {
                 flow.collect(onEvent)
             }
-        }
-    }
-}
-
-@Composable
-fun OnResume(
-    onResume: () -> Unit
-) {
-    OnLifecycleEvent(
-        onEvent = { _, event ->
-            onResume().takeIf { event == Lifecycle.Event.ON_RESUME }
-        }
-    )
-}
-
-@Composable
-private fun OnLifecycleEvent(
-    onEvent: (owner: LifecycleOwner, event: Lifecycle.Event) -> Unit
-) {
-    val eventHandler = rememberUpdatedState(onEvent)
-    val lifecycleOwner = rememberUpdatedState(LocalLifecycleOwner.current)
-
-    DisposableEffect(lifecycleOwner.value) {
-        val lifecycle = lifecycleOwner.value.lifecycle
-        val observer = LifecycleEventObserver { owner, event ->
-            eventHandler.value(owner, event)
-        }
-        lifecycle.addObserver(observer)
-        onDispose {
-            lifecycle.removeObserver(observer)
         }
     }
 }
--- app/src/main/kotlin/ru/mercury/vpclient/shared/ui/ktx/NotificationKtx.kt
+++ app/src/main/kotlin/ru/mercury/vpclient/shared/ui/ktx/NotificationKtx.kt
@@ -7,11 +7,8 @@ import android.os.Build
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 
-val Context.notificationManager: NotificationManagerCompat
-    get() = NotificationManagerCompat.from(this)
-
 val Context.isPostNotificationsPermissionGranted: Boolean
     get() = when {
         Build.VERSION.SDK_INT >= 33 -> ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
-        else -> notificationManager.areNotificationsEnabled()
+        else -> NotificationManagerCompat.from(this).areNotificationsEnabled()
     }
